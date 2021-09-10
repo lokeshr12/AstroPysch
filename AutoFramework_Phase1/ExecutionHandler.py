8c8f0d187187d857f7a64fb4aa8765bc32673401
delroy2826 <oliveiradelroy@gmail.com>
2021-09-10 15:58:33 +0530

    Execution Handler setup and initiater setup

---
+++ AutoFramework_Phase1/ExecutionHandler.py
@@ -0,0 +1,26 @@
+import config
+from WebAutomationTests.global_local.testcases.test_TestScenarioExecution import test_Initiator_global_local
+
+
+with open("config.py","r") as readConfig:
+    localtorun=""
+    excelToRun={}
+    readConfig=readConfig.read().split("\n")
+    excutionSheet=""
+    
+    for line in readConfig:
+        if "locals" in line:
+            localtorun="".join(line.split("=")[1].replace('"',""))  
+            excutionSheet=localtorun.split(",")
+            if config.executionType:
+                break
+        
+    for line in readConfig:
+        for i in range(len(excutionSheet)):
+            if "execution_"+excutionSheet[i] in line and "locals" not in line:
+                excelToRun[excutionSheet[i]]=line.split("=")[1].replace('"',"")
+                if config.executionType:
+                    break
+            
+    testExecutor ="test_Initiator_"+localtorun
+    globals()[testExecutor](excelToRun[localtorun])
